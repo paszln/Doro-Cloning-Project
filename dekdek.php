10f92997572ccf7da93f78bb55a42707c6eda86a
inearn-arsr <earn-br-100@hotmail.com>
2020-11-04 18:22:21 +0700

    dekdek

---
+++ dekdek.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>DEK DEK's GALLERY | D'ORO COFFEE</title>
+    <style>
+        body {
+            font-family: 'Mitr', sans-serif;
+        }
+    
+        .nav-item {
+            font-size: 14px;
+        }
+    
+        .nav-link {
+            color: white;
+        }
+    
+        .nav-link:hover {
+            color: #FA9773;
+        }
+    
+        h5 {
+            font-size: 16px;
+        }
+    
+        #img1 {
+            width: 224px;
+            height: 84px;
+            margin: 20px;
+        }
+    
+        #line {
+            width: 426px;
+            height: 50px;
+            border-top: 2px solid #547478;
+        }
+    
+        a {
+            color: white;
+        }
+    
+        a:hover {
+            color: #FA9773;
+        }
+    
+        .dropbtn {
+            background-color: #547478;
+            color: white;
+            font-size: 14px;
+            border: none;
+            cursor: pointer;
+        }
+    
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
+    
+        .dropdown-content {
+            border-radius: 3px;
+            opacity: 0.7;
+            background-color: #547478;
+            display: none;
+            position: absolute;
+            right: 0;
+            min-width: 200px;
+        }
+    
+        .dropdown-content a {
+            color: white;
+            text-decoration: none;
+        }
+    
+        .dropdown-content a:hover {
+            color: #FA9773;
+        }
+    
+        .dropdown:hover .dropdown-content {
+            display: block;
+            color: white;
+        }
+    
+        /* CSS for "Coming soon" */
+        #whitespace_for_footer {
+            width: 500px;
+            height: 420px;
+            margin-top: 80px;
+            color: #c14e25;
+        }
+        img{
+            width: 100%; height: 100%; object-fit: cover
+        }
+        .row{
+            margin-bottom: 15px;
+        }
+    </style>
+</head>
+<body>
+    <div style="color: white;">
+        <div class="top" style="font-size:30px; background-color: #104751;">
+            <center><img id="img1"
+                    src="https://static.wixstatic.com/media/9fa84d_aa13b15690be45ba91c57715ccd883ae~mv2_d_3508_2480_s_4_2.png/v1/fill/w_280,h_105,al_c,q_85,usm_0.66_1.00_0.01/9fa84d_aa13b15690be45ba91c57715ccd883ae~mv2_d_3508_2480_s_4_2.webp">
+            </center>
+        </div>
+    </div>
+
+    <div style="color: white;">
+        <ul class="nav justify-content-center navbar-light" style="background-color: #547478;">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.html" >HOME</a>
+            </li>
+
+            <div class="nav-item">
+                <li class="nav-link">
+                    <div class="dropdown">
+                        <button class="dropbtn">COMPANY</button>
+                        <ul class="dropdown-content">
+                            <a href="aboutus.html">ABOUT US</a><br>
+                            <a href="fromfarmtocup.html">FORM FARM TO CUP</a>
+                        </ul>
+                    </div>
+                </li>
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="product&services.html">PRODUCT & SERVICES</a>
+            </li>
+
+            <div class="nav-item">
+                <li class="nav-link">
+                    <div class="dropdown">
+                        <button class="dropbtn">NEWS&PROMOTION</button>
+                        <ul class="dropdown-content">
+                            <a href="#">NEWS & ACTIVITIES</a><br>
+                            <a href="#">PROMOTIONS</a><br>
+                            <a style="color: #FA9773;" href="#">DEK DEK's GALLERY</a><br>
+                            <a href="19th-anniversary.html">19th ANNIVERSARY</a>
+                        </ul>
+                    </div>
+                </li>
+            </div>
+
+            <div class="nav-item">
+                <li class="nav-link">
+                    <div class="dropdown">
+                        <button class="dropbtn">MEMBERSHIP</button>
+                        <ul class="dropdown-content">
+                            <a href="#">EARN & REDEEM</a><br>
+                            <a href="#">PREMIUM COLLECTION</a><br>
+                            <a href="#">MEMBERTIERS</a><br>
+                            <a href="#">REGISTER NOW</a>
+                        </ul>
+                    </div>
+                </li>
+            </div>
+
+            <li class="nav-item ">
+                <a class="nav-link " href="# ">STORE LOCATIONS</a>
+            </li>
+
+            <li class="nav-item ">
+                <a class="nav-link " href="# ">FRANCHISE</a>
+            </li>
+
+            <div class="nav-item">
+                <div class="dropdown">
+                    <li class="nav-link">
+                        <button class="dropbtn">CONTACT</button>
+                        <ul class="dropdown-content">
+                            <a href="#">CONTACT US</a><br>
+                            <a href="#">WORK WITH US</a>
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </ul>
+    </div>
+
+    <!--gallery-->
+    <div class="container" style="width: 1000px;">
+        <br>
+    
+    <?php
+        
+    ?>
+        <!-- <div class="row">
+            <div class="col-4">
+                <img src="https://static.wixstatic.com/media/9fa84d_b74bc9c7cf2f4e2daf80d8f38d2d4b4d~mv2_d_2340_1654_s_2.jpg/v1/fill/w_1104,h_781,fp_0.50_0.50,q_90,usm_12.60_0.42_0.19/9fa84d_b74bc9c7cf2f4e2daf80d8f38d2d4b4d~mv2_d_2340_1654_s_2.webp">
+            </div>
+            <div class="col-8">
+                <img src="https://static.wixstatic.com/media/9fa84d_23328beb3e194fc2839dcc6e9b1e448a~mv2_d_2340_1654_s_2.jpg/v1/fill/w_1104,h_781,fp_0.50_0.50,q_90,usm_12.60_0.42_0.19/9fa84d_23328beb3e194fc2839dcc6e9b1e448a~mv2_d_2340_1654_s_2.webp">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8">
+                <img src="https://static.wixstatic.com/media/9fa84d_7f7d7c61c3584a25901a6c7cc030aac7~mv2_d_2340_1654_s_2.jpg/v1/fill/w_1104,h_781,fp_0.50_0.50,q_90,usm_12.60_0.42_0.19/9fa84d_7f7d7c61c3584a25901a6c7cc030aac7~mv2_d_2340_1654_s_2.webp">
+            </div>
+            <div class="col-4">
+                <img src="https://static.wixstatic.com/media/9fa84d_69908e9998ae415ab9f4c7bfa451e497~mv2_d_2340_1654_s_2.jpg/v1/fill/w_1104,h_781,fp_0.50_0.50,q_90,usm_12.60_0.42_0.19/9fa84d_69908e9998ae415ab9f4c7bfa451e497~mv2_d_2340_1654_s_2.webp">
+            </div>
+        </div> -->
+
+        
+
+    </div>
+
+</body>
+</html>
